956c59c5bf9b3cc3430f2cfcaac0c4586ddafe1f
Mahmoud275 <medohl918@gmail.com>
2018-09-12 11:17:54 +0200

    läste in användarens namn

--- src/Läxa1.java
+++ src/Läxa1.java
@@ -4,5 +4,13 @@ public class Läxa1 {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		System.out.println("vad heter du");
+		
+		String namn = input.nextLine();
+		
+	
+	
+	
 	}
+	
 }
