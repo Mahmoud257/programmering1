da005a794b949ec3c1bcbfec51a0580d405aab0b
Mahmoud275 <medohl918@gmail.com>
2018-09-28 14:16:23 +0200

    träna

---
+++ src/kattis/fizzbuzz.java
@@ -0,0 +1,21 @@
+package kattis;
+
+import java.util.Scanner;
+
+import com.sun.corba.se.spi.orbutil.fsm.Input;
+
+public class fizzbuzz {
+
+public static void main(String[] args) {
+	
+scanner in = new Scanner(system.in);
+	// läs in tal 1	
+	int tal1 = Input.nextint(); 
+	int 
+
+
+
+
+}
+
+}
---
+++ src/kattis/timeloop.java
@@ -0,0 +1,6 @@
+package kattis;
+
+public class timeloop {
+ public static void main(String[] args) {
+	 Scanner 
+ }
